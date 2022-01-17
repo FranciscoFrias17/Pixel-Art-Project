73cf46689e26e06e83b6461835000a723f54ade4
FranciscoFrias17 <96884284+FranciscoFrias17@users.noreply.github.com>
2022-01-16 19:06:23 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -4,34 +4,36 @@ const myForm = document.getElementById('sizePicker');
 const canvas = document.getElementById('pixelCanvas');
 
 myForm.addEventListener('submit', function(evt) {
-    evt.preventDefault();
+	evt.preventDefault();
 
-//value for grid height
+	//value for grid height
 
-const height = document.getElementById('inputHeight').value;
+	const height = document.getElementById('inputHeight').value;
 
-//value for grid width
+	//value for grid width
 
-const width = document.getElementById('inputWidth').value;
+	const width = document.getElementById('inputWidth').value;
 
-makeGrid(height, width);
+	makeGrid(height, width);
 })
 
 function makeGrid(h, w) {
-//Remove previous tables
+	//Remove previous tables
 
-canvas.innerHTML="";
+	canvas.innerHTML = "";
 
-//Create new table
-for (let i = 0; i < h; i++) {
-    const canvasRow = canvas.insertRow(i);
+	//Creates a grid of squares
+    //Parameter for width - number of squares representing width of the grid
+    //Parameter for height - number of squares representing height of the grid
+	for (let i = 0; i < h; i++) {
+		const canvasRow = canvas.insertRow(i);
 
-for (let j = 0; j < w; j++) {
-    const canvasColumn = canvasRow.insertCell(j);
-        }
-    }
+		for (let j = 0; j < w; j++) {
+			const canvasColumn = canvasRow.insertCell(j);
+		}
+	}
 }
-
+    //Click event to fill in table cells
 canvas.addEventListener('click', function(evt) {
-    evt.target.style.backgroundColor = document.getElementById('colorPicker').value;
+	evt.target.style.backgroundColor = document.getElementById('colorPicker').value;
 })
